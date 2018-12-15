a08868c58f0985d98d8cc0aa3ee021e47f94b65c
Jerry Martinez <jerry@jmweb.net>
2018-12-15 00:16:17 -0500

    Minor refactor.

--- index.js
+++ index.js
@@ -17,17 +17,12 @@ const pattern = /^\s*(\d+)(?:\s*;(?:\s*url\s*=)?\s*(?:["']\s*(.*?)\s*['"]|(.*?))
  */
 const parseMetaRefresh = content =>
 {
-	const result = { timeout: null, url: null };
 	content = pattern.exec( content );
 
 	if( content !== null )
-	{
 		// pattern gaurantees first matching group
-		result.timeout = parseInt( content[ 1 ] );
-		result.url     = content[ 2 ] || content[ 3 ] || null;
-	}
-
-	return result;
+		return { timeout: parseInt( content[ 1 ] ), url: content[ 2 ] || content[ 3 ] || null };
+		return { timeout: null                    , url: null                                 };
 };
 
 module.exports = parseMetaRefresh;
